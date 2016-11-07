830e6da1204a290e014ece0f9a755509ea4b0607
Pieter Joost van de Sande <pj@born2code.net>
2016-11-07 09:13:12 +0100

    redirect request body to command stdin

--- main.go
+++ main.go
@@ -17,7 +17,12 @@ type CommandHandler struct {
 
 func (this *CommandHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	cmd := exec.Command(this.Command, this.Args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = request.Body
+
 	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
 		http.Error(response, err.Error(), 500)
 		return
 	}
@@ -30,20 +35,20 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "address",
-			Value: ":8080",
-			Usage: "the http addres to bind to, for example: \"127.0.0.1:8000\"",
+			Value: "0.0.0.0:8080",
+			Usage: "the address to listen on, for example: \"127.0.0.1:8080\"",
 		},
 	}
 	app.Action = func(ctx *cli.Context) error {
 		if len(ctx.Args()) == 0 {
-			return fmt.Errorf("missing command")
+			return fmt.Errorf("please specify the command to run, for example:\n\thookie my-script.sh")
 		}
 
 		address := ctx.String("address")
 		command := ctx.Args()[0]
 		args := ctx.Args()[1:]
 
-		fmt.Printf("listening on %v\n", address)
+		fmt.Printf("servning on %v\n", address)
 
 		return http.ListenAndServe(address, handlers.LoggingHandler(os.Stdout, &CommandHandler{
 			Command: command,
