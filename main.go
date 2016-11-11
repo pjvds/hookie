4af395b7b9e3fb6627e21aa25e8501a7f8eb845a
Pieter Joost van de Sande <pj@born2code.net>
2016-11-11 18:54:59 +0100

    add github-secret flag

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ func main() {
 			Value: "0.0.0.0:8080",
 			Usage: "the address to listen on, for example: \"127.0.0.1:8080\"",
 		},
+		cli.StringFlag{
+			Name:  "github-secret",
+			Usage: "validates the incoming request with the X-Hub-Signature header value set by Github",
+		},
 	}
 	app.Action = func(ctx *cli.Context) error {
 		if len(ctx.Args()) == 0 {
@@ -30,11 +34,20 @@ func main() {
 		args := ctx.Args()[1:]
 
 		fmt.Printf("servning on %v\n", address)
-
-		return http.ListenAndServe(address, handlers.LoggingHandler(os.Stdout, &CommandHandler{
+    handler := &CommandHandler{
 			Command: command,
 			Args:    args,
-		}))
+		}
+
+    if secret := ctx.String("github-secret"); len(secret) > 0{
+      fmt.Printf("github signature validation enabled\n")
+      handler := &GithubSecretValidator{
+        Secret: []byte(secret),
+        Handler: handler,
+      }
+    }
+
+		return http.ListenAndServe(address, handlers.LoggingHandler(os.Stdout, handlers.LoggingHandler(os.Stdout, handler))
 	}
 
 	app.Run(os.Args)
